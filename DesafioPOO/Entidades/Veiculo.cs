2236a2f0c575fbe9eeb74857f9a98b11181e4bf2
sthevanbello <sthevan.alves@gmail.com>
2022-08-02 17:16:08 -0300

    Classe Veículo - Criadas as propriedades e os métodos para retornar as informações do veículo

--- DesafioPOO/Entidades/Veiculo.cs
+++ DesafioPOO/Entidades/Veiculo.cs
@@ -8,5 +8,30 @@ namespace DesafioPOO.Entidades
 {
     public class Veiculo
     {
+        public Veiculo(string placa, string modelo, string cor)
+        {
+            Placa = placa;
+            Modelo = modelo;
+            Cor = cor;
+        }
+
+        public string Placa { get; set; }
+        public string Modelo { get; set; }
+        public string Cor { get; set; }
+
+        public string ObterPlaca()
+        {
+            return Placa;
+        }
+
+        public string ObterModelo()
+        {
+            return Modelo;
+        }
+
+        public string ObterCor()
+        {
+            return Cor;
+        }
     }
 }
