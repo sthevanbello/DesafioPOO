c3bc8aa3f0f6f0f8b2f79279abeca01555cca703
sthevanbello <sthevan.alves@gmail.com>
2022-08-02 20:08:15 -0300

    Classe Usuario - Adicionada a propriedade Senha. - Criado o método virtual EfetuarLogin

--- DesafioPOO/Entidades/Usuario.cs
+++ DesafioPOO/Entidades/Usuario.cs
@@ -8,10 +8,11 @@ namespace DesafioPOO.Entidades
 {
     public abstract class Usuario
     {
-        public Usuario(string login, string nome, string email, string telefone, string cpf)
+        public Usuario(string login, string nome, string email, string telefone, string cpf, string senha)
         {
             Id = new Guid();
             Login = login;
+            Senha = senha;
             Nome = nome;
             Email = email;
             Telefone = telefone;
@@ -20,9 +21,15 @@ namespace DesafioPOO.Entidades
 
         public Guid Id { get; set; }
         public string Login { get; set; }
+        public string Senha { get; private set; }
         public string Nome { get; set; }
         public string Email { get; set; }
         public string Telefone { get; set; }
         public string CPF { get; set; }
+
+        public virtual bool EfetuarLogin(string login, string senha)
+        {
+            return false;
+        }
     }
 }
