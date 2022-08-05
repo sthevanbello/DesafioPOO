12db961f4784a796bb033ce24e26e3142bc1c50f
sthevanbello <sthevan.alves@gmail.com>
2022-08-04 23:29:36 -0300

    Implementado o método de receber pagamento

--- DesafioPOO/Entidades/ContaBancaria.cs
+++ DesafioPOO/Entidades/ContaBancaria.cs
@@ -1,29 +1,42 @@
-﻿using System;
+﻿using DesafioPOO.Auxiliares;
+using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace DesafioPOO.Entidades
 {
     public class ContaBancaria
     {
-        public ContaBancaria(string agencia, string nomeDoBanco, string numeroDaConta, decimal saldoParaReceber)
+        public ContaBancaria(string agencia, string nomeDoBanco, string numeroDaConta, decimal saldo)
         {
             Agencia = agencia;
             NomeDoBanco = nomeDoBanco;
             NumeroDaConta = numeroDaConta;
-            SaldoParaReceber = saldoParaReceber;
+            Saldo = saldo;
         }
 
         private string Agencia { get; set; }
         private string NomeDoBanco { get; set; }
         private string NumeroDaConta { get; set; }
-        private decimal SaldoParaReceber { get; set; }
+        private decimal Saldo { get; set; }
 
+        /// <summary>
+        /// Recebe pagamento na conta do motorista
+        /// </summary>
+        /// <param name="valor"></param>
+        /// <returns></returns>
         public string ReceberPagamento(decimal valor)
         {
-            return "Pagamento recebido com sucesso";
+            Saldo += valor;
+            Thread.Sleep(1000);
+            Console.WriteLine();
+            Auxiliar.ExibirDivisoriaHorizontal("$-");
+            Thread.Sleep(1000);
+            return $"\nPagamento de {Saldo.ToString("C", CultureInfo.GetCultureInfo("pt-br"))} recebido com sucesso";
         }
     }
 }
