083ed60e27fdc8275937fc57f67129b816be5f0d
sthevanbello <sthevan.alves@gmail.com>
2022-08-04 23:28:56 -0300

    Modificado o método ToString

--- DesafioPOO/Entidades/CartaoDeCredito.cs
+++ DesafioPOO/Entidades/CartaoDeCredito.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using DesafioPOO.Enums;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,13 +9,17 @@ namespace DesafioPOO.Entidades
 {
     public class CartaoDeCredito : Cartao
     {
-        public CartaoDeCredito(string numeroDoCartao, string cpf, int cvv, DateTime validade) : base(numeroDoCartao, cpf, cvv, validade)
+        public CartaoDeCredito(string numeroDoCartao, string cpf, int cvv, string validade, string nomeDoCartao) : base(numeroDoCartao, cpf, cvv, validade, nomeDoCartao)
         {
+            Tipo = ETipoPagamento.Credito;
         }
-
         public string Pagar()
         {
             return "Pagamento com cartão de crédito efetuado com sucesso";
+        }
+        public override string ToString()
+        {
+            return $"\n- Cartão de crédito\nNúmero: {NumeroDoCartao}\nCpf: {Cpf}\nDígito verificador: {Cvv}\nValidade: {Validade}\n";
         }
     }
 }
--- DesafioPOO/Entidades/CartaoDeDebito.cs
+++ DesafioPOO/Entidades/CartaoDeDebito.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using DesafioPOO.Enums;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,13 +9,17 @@ namespace DesafioPOO.Entidades
 {
     public class CartaoDeDebito : Cartao
     {
-        public CartaoDeDebito(string numeroDoCartao, string cpf, int cvv, DateTime validade) : base(numeroDoCartao, cpf, cvv, validade)
+        public CartaoDeDebito(string numeroDoCartao, string cpf, int cvv, string validade, string nomeDoCartao) : base(numeroDoCartao, cpf, cvv, validade, nomeDoCartao)
         {
+            Tipo = ETipoPagamento.Debito;
         }
-
         public string Pagar()
         {
-            return "Pagamento com cartão de débito efetuado com sucesso";
+            return "Pagamento com cartão de crédito efetuado com sucesso";
+        }
+        public override string ToString()
+        {
+            return $"\n- Cartão de débito\nNúmero: {NumeroDoCartao}\nCpf: {Cpf}\nDígito verificador: {Cvv}\nValidade: {Validade}\n";
         }
     }
 }
